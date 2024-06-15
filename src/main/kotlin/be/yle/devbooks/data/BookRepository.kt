24380abd4e03161cc5e4d21b8b890bd877591a69
Yannick Lemin <yle@techyos.com>
2024-06-15 13:51:27 +0200

    book ids starting from 0

--- src/main/kotlin/be/yle/devbooks/data/BookRepository.kt
+++ src/main/kotlin/be/yle/devbooks/data/BookRepository.kt
@@ -5,9 +5,7 @@ import org.springframework.stereotype.Repository
 
 @Repository
 class BookRepository {
-    fun findAll(): List<Book> {
-        return ALL_BOOKS
-    }
+    fun findAll() = ALL_BOOKS
 
     fun findById(id: Int): Book? {
         return ALL_BOOKS.find { it.id == id }
@@ -15,11 +13,11 @@ class BookRepository {
 
     companion object {
         val ALL_BOOKS = listOf(
-            Book(1, "Clean Code", "Robert Martin", 2008),
-            Book(2, "The Clean Coder", "Robert Martin", 2011),
-            Book(3, "Clean Architecture", "Robert Martin", 2017),
-            Book(4, "Test Driven Development by Example", "Kent Beck", 2003),
-            Book(5, "Working Effectively With Legacy Code", "Michael C. Feathers", 2004),
+            Book(0, "Clean Code", "Robert Martin", 2008),
+            Book(1, "The Clean Coder", "Robert Martin", 2011),
+            Book(2, "Clean Architecture", "Robert Martin", 2017),
+            Book(3, "Test Driven Development by Example", "Kent Beck", 2003),
+            Book(4, "Working Effectively With Legacy Code", "Michael C. Feathers", 2004),
         )
     }
 }
