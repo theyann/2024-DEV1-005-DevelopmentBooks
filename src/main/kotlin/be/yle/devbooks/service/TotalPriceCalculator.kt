935046e4318029f8685ba6084b7deb1d831eefd4
Yannick Lemin <yle@techyos.com>
2024-06-16 21:38:38 +0200

    removed helper comment (no need anymore)

--- src/main/kotlin/be/yle/devbooks/service/TotalPriceCalculator.kt
+++ src/main/kotlin/be/yle/devbooks/service/TotalPriceCalculator.kt
@@ -11,19 +11,6 @@ class TotalPriceCalculator(
     private val splitter: SeriesSplitter
 ) {
 
-    /**
-     * Discount Rules:
-     * - 2 different books: 5%
-     * - 3 different books: 10%
-     * - 4 different books: 20%
-     * - 5 different books: 25%
-     * Note that if you buy, say, 4 books, of which 3 are different titles, you get a 10% discount on the 3 that form part of a set, but the 4th book still costs 50 EUR.
-     *
-     * Example:
-     * - 2 x Clean Code + 2 x Clean Coder + 2 x Clean Architecture + 1 x TDD By Example + 1 x Legacy Code = 320
-     * Reason : you have 2 times 4 different books --> 2 x ((4 x 50) - 20%) = 320
-     */
-
     fun calculateTotalPrice(basket: Basket): Double {
         val series = splitter.splitToSeries(basket)
 
